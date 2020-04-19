fe0fa039d03142e2f2f52b12f3d0148a2e257808
Poonam-raj <yourGitHub@email.com>
2020-04-19 18:33:31 +0100

    quiz javascript pulling through answer

--- js/quiz.js
+++ js/quiz.js
@@ -1,24 +1,36 @@
-ar scoreA = 0
-var scoreB = 0
-var scoreC = 0
+document.getElementById("quizForm").onsubmit = function() {
 
-function displayRadioValue() {
-    document.getElementById("result").innerHTML = "";
-    var ele = document.getElementsByTagName('input');
-    for (i = 0; i < ele.length; i++) {
-        if (ele[i].type = "radio") {
-            if (ele[i].checked)
-                document.getElementById("result").innerHTML += ele[i].name + " Value: " +
-                ele[i].value + "<br>";
-            if (ele[i].value = "A") {
-                return scoreA++
-            } else {
-                if (ele[i].value = "B") {
-                    return scoreB++
-                } else {
-                    return scoreC++
-                }
-            }
+
+        q1 = parseInt(document.querySelector('input[name = "q1"]:checked').value);
+
+        q2 = parseInt(document.querySelector('input[name = "q2"]:checked').value);
+
+        q3 = parseInt(document.querySelector('input[name = "q3"]:checked').value);
+
+        q4 = parseInt(document.querySelector('input[name = "q4"]:checked').value);
+
+        q5 = parseInt(document.querySelector('input[name = "q5"]:checked').value);
+
+        q6 = parseInt(document.querySelector('input[name = "q6"]:checked').value);
+
+        q7 = parseInt(document.querySelector('input[name = "q7"]:checked').value);
+
+        q8 = parseInt(document.querySelector('input[name = "q8"]:checked').value);
+
+        total = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8
+
+
+        if (total <= 8) {
+            document.getElementById("answer").innerHTML = "You got 'The Oblivious'.";
+        } else if (total > 8 && total <= 16) {
+            document.getElementById("answer").innerHTML = "You got 'The Facebook Expert'.";
+        } else if (total > 16 && total <= 24) {
+            document.getElementById("answer").innerHTML = "You got 'The Hoarder'.";
+        } else if (total > 24 && total <= 32) {
+            document.getElementById("answer").innerHTML = "You got 'The Motivated Self-Improver'.";
+        } else if (total > 32 && total <= 40) {
+            document.getElementById("answer").innerHTML = "You got 'The Hypochondriac'.";
         }
-    }
-}
+
+        return false; // required to not refresh the page; just leave this here
+    } // end the submit function
